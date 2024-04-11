8d53d339381dcda8b92006915816fdf569b7a4bd
Mihai Cojocaru <mihaicojocaru@Mihais-MacBook-Air.local>
2024-04-11 10:15:04 +0300

    custom_operators

---
+++ custom_operators/subtracting_a_string_from_another_string.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print('Foo Bar' - 'Foo');
+  print('Bar' - 'Bar');
+  print('Foo Bar' - 'Baz');
+}
+
+extension Remove on String {
+  String operator -(String other) => replaceAll(other, '');
+}
